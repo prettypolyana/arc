86ce44779fd0b37b077ac02a1c44a50f71041aa9
Polina Kulik <pollyandrrrr@yandex.ru>
2023-08-31 18:00:21 +0300

    feat: сделать промежуточный гамбургер

---
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,30 @@
+import './HamburgerMenu.css';
+import logo from '../../images/logo.svg'
+
+function HamburgerMenu() {
+    return (
+        <div className='toggle__hamburger'>
+            <input className='toggle__menu' type='checkbox' id='toggle__menu'/>
+            <label className='toggle__button' htmlFor='toggle__menu'>
+                <span></span>
+            </label>
+            <img className='toggle__logo' src={logo} alt='logo'/>
+            <ul className='toggle__box'>
+                <li className='toggle__item'><a className="toggle__link" href=''>О компании</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Наши посёлки</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Цены</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Уникальные участки</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>География работы</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Готовые решения</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Линейки домов</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Бизнес-планы</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Сервисная компания</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Новости проекты</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Успешные фермеры</a></li>
+                <li className='toggle__item'><a className="toggle__link" href=''>Контакты</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default HamburgerMenu;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
+
+function Header() {
+    return (
+        <>
+            <section className='header'>
+                <HamburgerMenu />
+            </section>
+        </>
+    )
+}
+
+export default Header;
